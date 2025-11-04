d6eefe6011de8decf4cb60fef496018ec15bfe33
Grikhanov Artem <artemgrihanov@ya.ru>
2025-11-05 01:36:33 +0300

    added sm errors in actions.c

--- fundalg/lr3.1/actions.c
+++ fundalg/lr3.1/actions.c
@@ -1,10 +1,19 @@
 #include "actions.h"
+#include <stdio.h>
 
 void printErrors(StatusCode status) {
   switch (status) {
 
   case INVALID_INPUT:
-    printf("ошибка, неверный ввод\n");
+    printf("неверный ввод\n");
+    break;
+
+  case INVALID_ARGS:
+    printf("переданы неверные аргументы\n");
+    break;
+
+  case BUFFER_TOO_SMALL:
+    printf("слишком маленький буффер\n");
     break;
 
   default:
