9d537721ac26e756e9985631afa0b32d13670cf8
Grikhanov Artem <artemgrihanov@ya.ru>
2025-11-05 01:34:38 +0300

    added new func in actions.h

--- fundalg/lr3.1/actions.h
+++ fundalg/lr3.1/actions.h
@@ -6,8 +6,13 @@
 typedef enum {
   OK = 0,
   INVALID_INPUT = 1,
+  INVALID_ARGS = 2,
+  BUFFER_TOO_SMALL = 3,
 } StatusCode;
 
 void printErrors(StatusCode status);
+
+StatusCode convertToBase2r(const unsigned int n, const int r, char *buffer,
+                           const size_t bufferSize);
 
 #endif
