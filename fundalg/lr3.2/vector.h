017fbda440b42d780e35d25ee1e62a736c878085
Grikhanov Artem <artemgrihanov@ya.ru>
2025-11-07 02:29:27 +0300

    added func declaration of vect

--- fundalg/lr3.2/vector.h
+++ fundalg/lr3.2/vector.h
@@ -3,6 +3,7 @@
 
 #include "actions.h"
 
+#include <cstddef>
 #include <stddef.h>
 
 #ifndef VECTOR_TYPE
@@ -21,5 +22,25 @@ typedef struct {
   VECTOR_TYPE (*copyVoidPtr)(VECTOR_TYPE);
   void (*deleteVoidPtr)(VECTOR_TYPE);
 } Vector;
+
+Vector create_vector(size_t initialCapacity,
+                     VECTOR_TYPE (*copyFunc)(VECTOR_TYPE),
+                     void (*deleteFunc)(VECTOR_TYPE));
+
+void erase_vector(Vector *);
+
+int is_equal_vector(const Vector *v1, const Vector *v2);
+
+void copy_vector(Vector *dest, const Vector *src);
+
+Vector *copy_vector_new(const Vector *src);
+
+void push_back_vector(Vector *v, VECTOR_TYPE value);
+
+void delete_at_vector(Vector *v, size_t index);
+
+VECTOR_TYPE get_at_vector(const Vector *v, size_t index);
+
+void delete_vector(Vector *v);
 
 #endif
