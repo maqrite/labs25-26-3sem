9a05ba071ad8cbc646e28cbe74e9da261f5e2642
Grikhanov Artem <artemgrihanov@ya.ru>
2025-11-10 11:33:18 +0300

    added check_brackets

--- fundalg/lr3.6/actions.c
+++ fundalg/lr3.6/actions.c
@@ -1,4 +1,7 @@
 #include "actions.h"
+#include "stack.h"
+
+#include <string.h>
 
 void printErrors(StatusCode status) {
   switch (status) {
@@ -31,4 +34,42 @@ bool areBracketsPaired(char open, char close) {
   }
 
   return false;
+}
+
+int check_brackets(const char *str) {
+  node *stack = NULL;
+  int len = strlen(str);
+  StatusCode status = OK;
+
+  for (int i = 0; i < len; i++) {
+    char c = str[i];
+
+    if (c == '(' || c == '{' || c == '[' || c == '<') {
+      push(&stack, c);
+    } else if (c == ')' || c == '}' || c == ']' || c == '>') {
+      if (isEmpty(&stack)) {
+        status = INVALID_INPUT;
+        break;
+      }
+
+      char top = (char)peek(&stack);
+
+      if (areBracketsPaired(top, c)) {
+        pop(&stack);
+      } else {
+        status = INVALID_INPUT;
+        break;
+      }
+    }
+  }
+
+  if (status == OK && !isEmpty(&stack)) {
+    status = INVALID_INPUT;
+  }
+
+  while (!isEmpty(&stack)) {
+    pop(&stack);
+  }
+
+  return status;
 }
