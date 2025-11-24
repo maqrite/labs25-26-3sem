6348703ba7702952414f60deb7972b545e009611
Grikhanov Artem <artemgrihanov@ya.ru>
2025-11-25 00:02:21 +0300

    определил функционал

--- fundalg/lr3.3/linkedList.h
+++ fundalg/lr3.3/linkedList.h
@@ -40,4 +40,47 @@ typedef struct {
   size_t size;
 } LinkedList;
 
+LinkedList create_list(void);
+void erase_list(LinkedList *list);
+void delete_list(LinkedList *list);
+void push_back_list(LinkedList *list, const LIST_TYPE *value);
+void push_front_list(LinkedList *list, const LIST_TYPE *value);
+LIST_TYPE *pop_back_list(LinkedList *list);
+LIST_TYPE *pop_front_list(LinkedList *list);
+void insert_at_list(LinkedList *list, size_t index, const LIST_TYPE *value);
+LIST_TYPE *delete_at_list(LinkedList *list, size_t index);
+LIST_TYPE *get_at_list(const LinkedList *list, size_t index);
+int is_equal_list(const LinkedList *l1, const LinkedList *l2);
+
+// stack
+void push_stack(LinkedList *stack, const LIST_TYPE *value);
+LIST_TYPE *pop_stack(LinkedList *stack);
+LIST_TYPE *peek_stack(const LinkedList *stack);
+
+// queue
+void enqueue(LinkedList *queue, const LIST_TYPE *value);
+LIST_TYPE *dequeue(LinkedList *queue);
+LIST_TYPE *peek_queue(const LinkedList *queue);
+
+// functions
+int read_file_to_list(const char *filename, LinkedList *list);
+int insert_sorted(LinkedList *list, LIST_TYPE *new_liver);
+Node *find_liver(const LinkedList *list, unsigned int id);
+void modify_liver(Node *node, const LIST_TYPE *new_data);
+int write_list_to_file(const char *filename, const LinkedList *list);
+void print_liver(const LIST_TYPE *liver);
+void print_list(const LinkedList *list);
+
+// undo mod
+
+typedef struct {
+  enum { MOD_ADD, MOD_DELETE, MOD_EDIT } type;
+  size_t index;
+  LIST_TYPE *old_data;
+  LIST_TYPE *new_data;
+} Modification;
+
+void undo_modifications(LinkedList *list, LinkedList *undo_stack);
+void push_modification(LinkedList *undo_dtack, Modification mod);
+
 #endif
