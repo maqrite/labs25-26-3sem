c460d4b40baaa203c4410d7516e4b7a0e54137df
Grikhanov Artem <artemgrihanov@ya.ru>
2025-11-26 19:27:39 +0300

    -

---
+++ fundalg/lr3.7/interpreter.c
@@ -0,0 +1,82 @@
+#include "interpreter.h"
+#include "actions.h"
+#include "stack.h"
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ll long long
+
+int getPrecedence(char op) {
+  if (op == '+' || op == '-') {
+    return 1;
+  }
+
+  if (op == '*' || op == '/') {
+    return 2;
+  }
+
+  if (op == '^') {
+    return 3;
+  }
+
+  return 0;
+}
+
+bool isOperator(char op) {
+  return op == '+' || op == '-' || op == '*' || op == '/' || op == '^';
+}
+
+StatusCode evaluateOpertion(long long operand1, long long operand2, char op,
+                            long long *result) {
+  switch (op) {
+  case '+':
+    *result = operand1 + operand2;
+    break;
+
+  case '-':
+    *result = operand1 - operand2;
+    break;
+
+  case '*':
+    *result = operand1 * operand2;
+    break;
+
+  case '/':
+    if (operand2 == 0) {
+      return DIVISION_BY_ZERO;
+    }
+    *result = operand1 / operand2;
+    break;
+
+  case '^':
+    if (operand2 < 0) {
+      if (operand1 == 1) {
+        *result = 1;
+      } else if (operand1 == -1) {
+        *result = (operand2 % 2 == 0) ? 1 : -1;
+      } else {
+        *result = 0;
+      }
+    } else {
+      ll res = 1;
+      ll base = operand1;
+      ll exp = operand2;
+      while (exp > 0) {
+        if (exp % 2 == 1) {
+          res *= base;
+        }
+        base *= base;
+        exp /= 2;
+      }
+      *result = res;
+    }
+    break;
+
+  default:
+    return UNEXPECTED_TOKEN;
+  }
+
+  return OK;
+}
