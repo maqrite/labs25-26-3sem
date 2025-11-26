477d2eb4b1947f408ebb5cf6a164b2030a0ccac0
Grikhanov Artem <artemgrihanov@ya.ru>
2025-11-26 19:19:36 +0300

    -

---
+++ fundalg/lr3.7/interpreter.h
@@ -0,0 +1,21 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include "actions.h"
+#include <stdbool.h>
+
+#define VAR_COUNT 26
+
+typedef struct {
+  long long variables[VAR_COUNT];
+  bool isInit[VAR_COUNT];
+  FILE *logFile;
+  int commandCount;
+} InterpreterState;
+
+StatusCode initializeInterpreter(InterpreterState *state,
+                                 const char *logFileName);
+StatusCode precessCommand(InterpreterState *state, const char *commandLine);
+void destroyInterpreter(InterpreterState &state);
+
+#endif
