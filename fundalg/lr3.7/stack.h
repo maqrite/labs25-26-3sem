e735ddcf7ba510c07a40c4ffafee3391a158e698
Grikhanov Artem <artemgrihanov@ya.ru>
2025-11-26 19:10:32 +0300

    aboba

---
+++ fundalg/lr3.7/stack.h
@@ -0,0 +1,32 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "actions.h"
+#include <stdbool.h>
+
+#define ll long long
+
+typedef union {
+  ll value;
+  char op;
+} StackData;
+
+typedef struct StackNode {
+  StackData data;
+  struct StackNode *next;
+} StackNode;
+
+typedef struct {
+  StackNode *top;
+} Stack;
+
+void initializeStack(Stack *s);
+bool isEmptyStack(const Stack *s);
+StatusCode push(Stack *s, StackData data);
+StatusCode pop(Stack *s, StackData *data);
+StatusCode peek(const Stack *s, StackData *data);
+void destroyStack(Stack *s);
+
+#endif
+
+#endif
